78e5165cba2269fb59d93dbe1ff8b2d4a421fcb2
Scott Pakin <scott+gpg@pakin.org>
2022-12-29 21:33:26 -0700

    Write a unit test for the "full" API

---
+++ ll_test.go
@@ -0,0 +1,57 @@
+// This file tests the high package's low-level API wrappers.
+
+package highs
+
+import "testing"
+
+// TestFullAPIMin mimics the first test in HiGHS's full_api function from
+// examples/call_highs_from_c.c:
+//
+//	Min    f  =  x_0 +  x_1 + 3
+//	s.t.                x_1 <= 7
+//	       5 <=  x_0 + 2x_1 <= 15
+//	       6 <= 3x_0 + 2x_1
+//	0 <= x_0 <= 4; 1 <= x_1
+func TestFullAPIMin(t *testing.T) {
+	// Define a function that calls another function and aborts the test if
+	// it returns a non-nil error.
+	checkErr := func(e error) {
+		if e != nil {
+			t.Fatal(e)
+		}
+	}
+	
+	// Prepare the model.
+	model := NewRawModel()
+	checkErr(model.SetBoolOption("output_flag", false))
+	checkErr(model.SetMaximization(false)) // Unnecessary but included for testing
+	checkErr(model.SetOffset(3.0))
+	checkErr(model.AddColumnBounds([]float64{0.0, 1.0},
+		[]float64{4.0, 1.0e30}))
+	checkErr(model.SetColumnCosts([]float64{1.0, 1.0}))
+	checkErr(model.AddCompSparseRows([]float64{-1.0e30, 5.0, 6.0},
+		[]int{0, 1, 3}, []int{1, 0, 1, 0, 1}, []float64{1.0, 1.0, 2.0, 3.0, 2.0},
+		[]float64{7.0, 15.0, 1.0e30}))
+
+	// Solve the model.
+	soln, err := model.Solve()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if soln.Status != Optimal {
+		t.Fatalf("solve returned %s instead of Optimal", soln.Status)
+	}
+
+	// Confirm that each field is as expected.
+	compSlices(t, "ColumnPrimal", soln.ColumnPrimal, []float64{0.5, 2.25})
+	compSlices(t, "RowPrimal", soln.RowPrimal, []float64{2.25, 5.0, 6.0})
+	compSlices(t, "ColumnDual", soln.ColumnDual, []float64{0.0, 0.0})
+	compSlices(t, "RowDual", soln.RowDual, []float64{0.0, 0.25, 0.25})
+	compSlices(t, "ColumnBasis", soln.ColumnBasis, []BasisStatus{Basic, Basic})
+	compSlices(t, "RowBasis", soln.RowBasis, []BasisStatus{Basic, Lower, Lower})
+
+	// Validate the objective value.
+	if soln.Objective != 5.75 {
+		t.Fatalf("objective value was %.2f but should have been 5.75", soln.Objective)
+	}
+}
