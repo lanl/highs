fb1b4804a441122a7bdbfd6cf87a83a811ff10c0
Scott Pakin <scott+gpg@pakin.org>
2023-01-03 07:09:13 -0700

    Provide some package-level documentation

--- highs.go
+++ highs.go
@@ -1,4 +1,33 @@
-// Package highs provides a Go interface to the HiGHS optimizer.
+/*
+
+Package highs provides a Go interface to the HiGHS optimizer.  HiGHS
+(https://highs.dev/)—and the highs package—support linear programming (LP),
+mixed-integer programming (MIP)—also known as mixed-integer linear programming
+(MILP)—and quadratic programming (QP) models.
+
+The highs package provides both a low-level and a high-level interface to
+HiGHS.  The low-level interface, provided by the RawModel type, includes a rich
+set of methods that work directly with HiGHS's underlying, opaque data type.
+The high-level interface, provided by the Model type, is a simple struct whose
+fields the programmer can modify directly.  The ToRawModel method converts from
+a Model to a RawModel, enabling models to be specified conveniently at a high
+level then manipulated with the more featureful low-level API.
+
+In terms of the names of fields in Model, the highs package solves numerous
+variants of the following core problem:
+
+    Minimize    ColCosts ⋅ ColumnPrimal
+    subject to  RowLower ≤ CoeffMatrix ColumnPrimal ≤ RowUpper
+    and         ColLower ≤ ColumnPrimal ≤ ColUpper    
+
+where all variables in the above are vectors except for CoeffMatrix, which is a
+matrix.  "ColCosts ⋅ ColumnPrimal" denotes the inner product of those two
+vectors, and "CoeffMatrix ColumnPrimal" denotes matrix-vector multiplication.
+
+ColumnPrimal is a member of the Solution struct and is what the preceding
+formulation is solving for.
+
+*/   
 package highs
 
 // #cgo pkg-config: highs
