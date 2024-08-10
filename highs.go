454067f0ec81048e54bba290e53f8568087f3cc4
Scott Pakin <scott+gpg@pakin.org>
2024-08-10 17:03:20 -0600

    Run the source through `gofmt -s`

--- highs.go
+++ highs.go
@@ -16,9 +16,9 @@ low-level API.
 In terms of the names of fields in Model, the highs package solves numerous
 variants of the following core problem:
 
-    Minimize    ColCosts ⋅ ColumnPrimal
-    subject to  RowLower ≤ ConstMatrix ColumnPrimal ≤ RowUpper
-    and         ColLower ≤ ColumnPrimal ≤ ColUpper
+	Minimize    ColCosts ⋅ ColumnPrimal
+	subject to  RowLower ≤ ConstMatrix ColumnPrimal ≤ RowUpper
+	and         ColLower ≤ ColumnPrimal ≤ ColUpper
 
 where all variables in the above are vectors except for ConstMatrix, which is a
 matrix.  "ColCosts ⋅ ColumnPrimal" denotes the inner product of those two
